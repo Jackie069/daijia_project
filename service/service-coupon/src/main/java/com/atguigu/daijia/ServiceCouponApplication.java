303df2fddfffcefa59182f77d0fbf90dfa81760d
sztu202200202169 <1925597716@qq.com>
2025-03-15 20:29:03 +0800

    test1

--- service/service-coupon/src/main/java/com/atguigu/daijia/ServiceCouponApplication.java
+++ service/service-coupon/src/main/java/com/atguigu/daijia/ServiceCouponApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 public class ServiceCouponApplication {
-
+//test git
     public static void main(String[] args) {
         SpringApplication.run(ServiceCouponApplication.class, args);
     }
